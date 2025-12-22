c88af5bd089228c2ee6c8d4d4d94b8814b9d7584
Amine <amine@post.com>
2025-12-22 20:21:49 +0100

    Add some layouts

--- app/Model/ProductService.php
+++ app/Model/ProductService.php
@@ -9,9 +9,9 @@ final class ProductService
   public function getAll(): array
   {
     return [
-      ['name' => 'Produkt A', 'price' => 19.99],
-      ['name' => 'Produkt B', 'price' => 29.99],
-      ['name' => 'Produkt C', 'price' => 9.99],
+      ['name' => 'Produkt A', 'price' => 19.99, 'image' => '/images/product1.png'],
+      ['name' => 'Produkt B', 'price' => 29.99, 'image' => '/images/product2.png'],
+      ['name' => 'Produkt C', 'price' => 9.99, 'image' => '/images/product3.png'],
     ];
   }
 }
